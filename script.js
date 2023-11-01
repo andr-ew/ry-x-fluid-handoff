65a79affb4bef0d0974c26b8dd94cd0a26e04487
andr-ew <andrewcshike@gmail.com>
2023-11-01 14:37:10 -0500

    start

--- script.js
+++ script.js
@@ -78,7 +78,10 @@ function pointerPrototype() {
 
 let pointers = [];
 let splatStack = [];
-pointers.push(new pointerPrototype());
+
+let mousePointerIdx = pointers.push(new pointerPrototype()) - 1;
+let densityPointerIdx = pointers.push(new pointerPrototype()) - 1;
+let velocityPointerIdx = pointers.push(new pointerPrototype()) - 1;
 
 const { gl, ext } = getWebGLContext(canvas);
 
@@ -1812,13 +1815,13 @@ function correctRadius(radius) {
 canvas.addEventListener('mousedown', e => {
     let posX = scaleByPixelRatio(e.offsetX);
     let posY = scaleByPixelRatio(e.offsetY);
-    let pointer = pointers.find(p => p.id == -1);
-    if (pointer == null) pointer = new pointerPrototype();
+    let pointer = pointers[mousePointerIdx];
+    // if (pointer == null) pointer = new pointerPrototype();
     updatePointerDownData(pointer, -1, posX, posY);
 });
 
 canvas.addEventListener('mousemove', e => {
-    let pointer = pointers[0];
+    let pointer = pointers[mousePointerIdx];
     if (!pointer.down) return;
     let posX = scaleByPixelRatio(e.offsetX);
     let posY = scaleByPixelRatio(e.offsetY);
@@ -1826,50 +1829,50 @@ canvas.addEventListener('mousemove', e => {
 });
 
 window.addEventListener('mouseup', () => {
-    updatePointerUpData(pointers[0]);
+    updatePointerUpData(pointers[mousePointerIdx]);
 });
 
-canvas.addEventListener('touchstart', e => {
-    e.preventDefault();
-    const touches = e.targetTouches;
-    while (touches.length >= pointers.length)
-        pointers.push(new pointerPrototype());
-    for (let i = 0; i < touches.length; i++) {
-        let posX = scaleByPixelRatio(touches[i].pageX);
-        let posY = scaleByPixelRatio(touches[i].pageY);
-        updatePointerDownData(
-            pointers[i + 1],
-            touches[i].identifier,
-            posX,
-            posY
-        );
-    }
-});
+// canvas.addEventListener('touchstart', e => {
+//     e.preventDefault();
+//     const touches = e.targetTouches;
+//     while (touches.length >= pointers.length)
+//         pointers.push(new pointerPrototype());
+//     for (let i = 0; i < touches.length; i++) {
+//         let posX = scaleByPixelRatio(touches[i].pageX);
+//         let posY = scaleByPixelRatio(touches[i].pageY);
+//         updatePointerDownData(
+//             pointers[i + 1],
+//             touches[i].identifier,
+//             posX,
+//             posY
+//         );
+//     }
+// });
 
-canvas.addEventListener(
-    'touchmove',
-    e => {
-        e.preventDefault();
-        const touches = e.targetTouches;
-        for (let i = 0; i < touches.length; i++) {
-            let pointer = pointers[i + 1];
-            if (!pointer.down) continue;
-            let posX = scaleByPixelRatio(touches[i].pageX);
-            let posY = scaleByPixelRatio(touches[i].pageY);
-            updatePointerMoveData(pointer, posX, posY);
-        }
-    },
-    false
-);
+// canvas.addEventListener(
+//     'touchmove',
+//     e => {
+//         e.preventDefault();
+//         const touches = e.targetTouches;
+//         for (let i = 0; i < touches.length; i++) {
+//             let pointer = pointers[i + 1];
+//             if (!pointer.down) continue;
+//             let posX = scaleByPixelRatio(touches[i].pageX);
+//             let posY = scaleByPixelRatio(touches[i].pageY);
+//             updatePointerMoveData(pointer, posX, posY);
+//         }
+//     },
+//     false
+// );
 
-window.addEventListener('touchend', e => {
-    const touches = e.changedTouches;
-    for (let i = 0; i < touches.length; i++) {
-        let pointer = pointers.find(p => p.id == touches[i].identifier);
-        if (pointer == null) continue;
-        updatePointerUpData(pointer);
-    }
-});
+// window.addEventListener('touchend', e => {
+//     const touches = e.changedTouches;
+//     for (let i = 0; i < touches.length; i++) {
+//         let pointer = pointers.find(p => p.id == touches[i].identifier);
+//         if (pointer == null) continue;
+//         updatePointerUpData(pointer);
+//     }
+// });
 
 window.addEventListener('keydown', e => {
     if (e.code === 'KeyP') config.PAUSED = !config.PAUSED;
@@ -2008,4 +2011,3 @@ function hashCode(s) {
     }
     return hash;
 }
-
